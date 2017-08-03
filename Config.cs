6db965ded0f3ac2375f5e41da6513b88091a36ac
Kamal <kamal@mixerp.com>
2017-08-03 22:51:40 +0545

    Routine development

--- Config.cs
+++ Config.cs
@@ -12,16 +12,19 @@ namespace InitiativeNepal
         public string ContentType { get; set; } = "application/x-www-form-urlencoded;";
 
         [Required]
-        public string Method { get; set; } = "GET";
+        public string Method { get; set; } = "POST";
 
         [Required]
-        public string ApiUrl { get; set; } = "http://beta.thesmscentral.com/api/v3/sms";
+        public string ApiUrl { get; set; } = "http://api.ininepal.com/api/index";
 
         [Required]
         public string SenderId { get; set; }
 
         [Required]
-        public string AuthenticationToken { get; set; }
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
 
         public string FromName { get; set; }
         public string FromNumber { get; set; }
